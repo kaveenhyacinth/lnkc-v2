7657f36c091b9cefb72821c454b11fdf7bf2a62a
kaveenhyacinth <alwiskaveen@gmail.com>
2023-08-14 08:53:26 +0530

    setup client side router

---
+++ packages/client/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import NotFoundErrorPage from "../containers/404";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundErrorPage />,
+  },
+  {
+    path: "/test",
+    element: <div>Tesing route</div>,
+  },
+]);
+
+export default router;
