31328aaf5e83f1703402383b1ba478a853369b86
kaveenhyacinth <alwiskaveen@gmail.com>
2023-08-14 07:43:56 +0530

    setup react app and config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,7 +3,28 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          "babel-plugin-macros",
+          [
+            "@emotion/babel-plugin-jsx-pragmatic",
+            {
+              export: "jsx",
+              import: "__cssprop",
+              module: "@emotion/react",
+            },
+          ],
+          [
+            "@babel/plugin-transform-react-jsx",
+            { pragma: "__cssprop" },
+            "twin.macro",
+          ],
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
     // proxy: {
